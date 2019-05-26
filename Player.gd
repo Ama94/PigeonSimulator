f9406eef6b8c2d47e0761a5fa92fc2a935676ce9
UJBCMJKB2018 <ujmjbckb2018@gmail.com>
2019-05-26 15:42:42 +0200

    Collision borders

--- Player.gd
+++ Player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends RigidBody2D
 
 const ACCELERATION = 10
 # Declare member variables here. Examples:
@@ -25,35 +25,47 @@ func _process(delta):
 	
 	
 func _physics_process(delta):
+	linear_velocity = linear_velocity.normalized()*min(linear_velocity.length(), 300.0)
 	process_input()
-	radial_acceleration *= 0.93
+#	radial_acceleration *= 0.93
 #	orientation = radial_acceleration
-	facing = Vector2( cos(orientation) , sin(orientation))
-	rotation = orientation
-	velocity *=0.95
-	position += velocity * delta + direction
-	position.y += gravity
-	velocity += force * delta
+#	facing = Vector2( cos(orientation) , sin(orientation))
+#	rotation = orientation
+#	velocity *=0.95
+#	position += velocity * delta + direction
+#	position.y += gravity
+#	velocity += force * delta
+	print(linear_velocity)
+	$Sprite.rotation = get_angle_to(linear_velocity+position)
+	
+	if(abs($Sprite.rotation_degrees) > 90):
+		 $Sprite.set_flip_v(true)
+	else:
+		$Sprite.set_flip_v(false)
+	$CollisionShape2D2.rotation = $Sprite.rotation+(PI/2)
 	if(value < 100.0):
 		value += 1
-	get_node("../cooldown").set_value(value)
-	self.texture = load("res://assets/pigeon_up.png")
+	get_node("../GUI/cooldown").set_value(value)
+	$Sprite.texture = load("res://assets/pigeon_up.png")
 	update()
 	
 	pass
 func process_input():
 	if Input.is_action_pressed("ui_right"):
-		direction = Vector2(2.0,0)
-		get_node("../Pigeon").set_flip_h(false)
+		apply_central_impulse(Vector2(10,0))
+#		direction = Vector2(2.0,0)
+#		$Sprite.set_flip_h(false)
 	if Input.is_action_pressed("ui_left"):
-		direction = Vector2(-2.0,0)
-		get_node("../Pigeon").set_flip_h(true)
+		apply_central_impulse(Vector2(-10,0))
+#		direction = Vector2(-2.0,0)
+#		$Sprite.set_flip_h(true)
 	if(Input.is_action_just_pressed("ui_up")):
-		position += Vector2(0,-15.0)
+		apply_impulse(Vector2.ZERO,Vector2(0,-200))
+#		position += Vector2(0,-15.0)
 #		self.texture = load("res://assets/pigeon_up.png")
 	if(Input.is_action_just_pressed("ui_select")):
-		if(get_node("../cooldown").get_value() == 100):
-			get_node("../cooldown").set_value(0)
+		if(get_node("../GUI/cooldown").get_value() == 100):
+			get_node("../GUI/cooldown").set_value(0)
 			value = 0
 			spawn_bullet()
 #	pass
